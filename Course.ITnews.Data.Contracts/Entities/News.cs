0ca791cea385b3da9d7e58179b21c6c9a10cf138
Nikita <elfenlied1990lol@gmail.com>
2019-05-15 10:52:57 +0300

    First commit

---
+++ Course.ITnews.Data.Contracts/Entities/News.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.ITnews.Data.Contracts.Entities
+{
+    class News
+    {
+    }
+}
